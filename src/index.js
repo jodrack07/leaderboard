ebd1ae32a5164bb22d330ee5e918fc784b4865b2
jodrack <drack.sir01@gmail.com>
2022-05-11 13:56:07 +0200

    Import some functions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './style.css';
 import { scores } from './modules/data.js';
+import { fetchData, createData } from './actions.js';
+import { showAlert } from './utils.js';
 
 const list = document.querySelector('.list');
 list.innerHTML = '';
