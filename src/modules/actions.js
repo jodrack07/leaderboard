e701472b09e58ae229ee561c39944a5cc4a52183
jodrack <drack.sir01@gmail.com>
2022-05-11 13:51:26 +0200

    Add fetchData function to get data from the API

--- src/modules/actions.js
+++ src/modules/actions.js
@@ -2,3 +2,9 @@ const BASE_URL =
   'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
 const gameId = 'i1UvxKrc5OCc9DmOVeKd';
 const URI = `${BASE_URL}games/${gameId}/scores/`;
+
+export const fetchData = async () => {
+  const response = await fetch(URI);
+  const data = await response.json();
+  get(data);
+};
