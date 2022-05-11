692334b0236c21ae301476d51b79d502dfd74a89
jodrack <drack.sir01@gmail.com>
2022-05-11 13:58:04 +0200

    Select elements to be used

--- src/index.js
+++ src/index.js
@@ -2,11 +2,3 @@ import './style.css';
 import { scores } from './modules/data.js';
 import { fetchData, createData } from './actions.js';
 import { showAlert } from './utils.js';
-
-const list = document.querySelector('.list');
-list.innerHTML = '';
-scores.forEach((score) => {
-  list.innerHTML += `
-           <p>Name: ${score}</p>
-    `;
-});
