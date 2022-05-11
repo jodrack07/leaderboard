927c5e5008d9a9ee58f38714318b680b44819511
jodrack <drack.sir01@gmail.com>
2022-05-11 13:58:45 +0200

    Implement the refresh functionality

--- src/index.js
+++ src/index.js
@@ -2,3 +2,11 @@ import './style.css';
 import { scores } from './modules/data.js';
 import { fetchData, createData } from './actions.js';
 import { showAlert } from './utils.js';
+
+const list = document.querySelector('.list');
+const submitButton = document.querySelector('.btn-submit');
+const refreshButton = document.querySelector('.btn-refresh');
+const user = document.querySelector('#playerName');
+const score = document.querySelector('#score');
+
+refreshButton.addEventListener('click', () => location.reload());
